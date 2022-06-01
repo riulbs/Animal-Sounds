89845606356e04480be5298e12db2c0acb781c35
lance <lance@DESKTOP-C48FFLL>
2022-06-01 21:50:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class zoo
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string animal = "dog";
+            string sound = "woof";
+
+            Console.WriteLine(animal + " makes " + sound);
+
+            Console.WriteLine("Press any key to close the console...");
+            Console.ReadKey();
+        }
+    }
+}
